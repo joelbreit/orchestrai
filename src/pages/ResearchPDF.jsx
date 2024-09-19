9a5724b4b4c423336c8ed82948be53d539c106d3
Joel Breit <121129520+joelbreit@users.noreply.github.com>
2024-09-19 15:04:32 -0400

    Back to using the public file

--- src/pages/ResearchPDF.jsx
+++ src/pages/ResearchPDF.jsx
@@ -1,41 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
-import PDF_FILE from "../assets/Article.pdf";
-
 const ResearchPDF = () => {
-	const [loadError, setLoadError] = useState(false);
-
-	const handleError = () => {
-		console.error("Failed to load the PDF document.");
-		setLoadError(true);
-	};
-
 	return (
 		<div>
 			<Header />
 			<div style={{ height: "100vh" }}>
-				{loadError ? (
-					<div style={{ textAlign: "center", padding: "20px" }}>
-						<p>
-							Failed to load the PDF document. Please try again
-							later.
-						</p>
-					</div>
-				) : (
-					<iframe
-						src={PDF_FILE}
-						title="Research Document"
-						style={{
-							width: "100%",
-							height: "100%",
-							border: "none",
-						}}
-						onError={handleError}
-					></iframe>
-				)}
+				<iframe
+					src="/Article.pdf"
+					title="Research Document"
+					style={{
+						width: "100%",
+						height: "100%",
+						border: "none",
+					}}
+				></iframe>
 			</div>
 			<Footer />
 		</div>
