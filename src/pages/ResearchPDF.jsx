4b0c72549a72cf35fa9ff88243b0098fe84d3d9d
Joel Breit <121129520+joelbreit@users.noreply.github.com>
2024-09-19 14:45:50 -0400

    PDF loading errors

--- src/pages/ResearchPDF.jsx
+++ src/pages/ResearchPDF.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+import PDF_FILE from "../assets/Article.pdf";
+
 const ResearchPDF = () => {
+	const [loadError, setLoadError] = useState(false);
+
+	const handleError = () => {
+		console.error("Failed to load the PDF document.");
+		setLoadError(true);
+	};
+
 	return (
 		<div>
 			<Header />
 			<div style={{ height: "100vh" }}>
-				<iframe
-					src="/Article.pdf"
-					title="Research Document"
-					style={{
-						width: "100%",
-						height: "100%",
-						border: "none",
-					}}
-				></iframe>
+				{loadError ? (
+					<div style={{ textAlign: "center", padding: "20px" }}>
+						<p>
+							Failed to load the PDF document. Please try again
+							later.
+						</p>
+					</div>
+				) : (
+					<iframe
+						src={PDF_FILE}
+						title="Research Document"
+						style={{
+							width: "100%",
+							height: "100%",
+							border: "none",
+						}}
+						onError={handleError}
+					></iframe>
+				)}
 			</div>
 			<Footer />
 		</div>
