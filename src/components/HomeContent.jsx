a810308e2f77088880e7c3170068584e0faec79c
Joel Breit <121129520+joelbreit@users.noreply.github.com>
2025-08-16 10:08:13 -0400

    Fixed a spelling error that's just been hanging out on the home page forever. Oops

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -32,10 +32,11 @@ const HomeContent = () => {
 					<h1>OrchestrAI</h1>
 				</div>
 				<p className="lead">
-					OrchestrAI is a tool that uses large language models to help generate and refine music compositions in ABC
-					notation. This notation style allows for ledgeable and
-					editable music that can be viewed and played in the browser,
-					shared with others, or exported to MIDI.
+					OrchestrAI is a tool that uses large language models to help
+					generate and refine music compositions in ABC notation. This
+					notation style allows for legible and editable music that
+					can be viewed and played in the browser, shared with others,
+					or exported to MIDI.
 				</p>
 				<Button
 					color="primary"
